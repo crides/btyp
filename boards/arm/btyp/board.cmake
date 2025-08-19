01e3f46dec29fb691b8a0d97ac56f7a5a832e5cd
crides <i@crid.es>
2025-08-19 08:49:51 -0700

    works; initial layout

--- boards/arm/btyp/board.cmake
+++ boards/arm/btyp/board.cmake
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: MIT
 
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+board_runner_args(pyocd "--target=nrf52840")
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
